a1589aa468c36c4770049382e3e6a8b9da723f3f
kiksun <kimera4173@gmail.com>
2019-12-10 23:32:48 +0900

    reduxとconnected-react-routerの実装

--- p2hacks/src/App.js
+++ p2hacks/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-import Confirm from './components/Confirm';
-function App() {
-  return (
-    <div className="App">
-      <Confirm/>
-    </div>
-  );
+import React from 'react'
+import { Route, Switch } from 'react-router'
+import PropTypes from 'prop-types'
+import { ConnectedRouter } from 'connected-react-router'
+import Confirm from './components/Confirm'
+
+const App = ({ history }) => {
+  return(
+    <ConnectedRouter history={history}>
+      <div>
+        <Switch>
+          <Route exact path="" component={Confirm} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
+  )
+}
+App.propTypes = {
+  history: PropTypes.object,
 }
 
-export default App;
+export default App
