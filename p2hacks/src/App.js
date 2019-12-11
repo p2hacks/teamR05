f214f28d1eaca72df6d8a4118721fcaf3fa14203
kiksun <kimera4173@gmail.com>
2019-12-11 19:27:29 +0900

    bug fix

--- p2hacks/src/App.js
+++ p2hacks/src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router'
 import PropTypes from 'prop-types'
 import { ConnectedRouter } from 'connected-react-router'
 import Confirm from './components/Confirm'
-import React from 'react';
 import Home from './components/Home';
 import './App.css';
 import End from './components/End';
@@ -13,10 +12,10 @@ const App = ({ history }) => {
     <ConnectedRouter history={history}>
       <div>
         <Switch>
-          <Route exact path="/" component={Home} />
-     <Route exact path="/Home" component={Home} />
-     <Route exact path="/Confirm" component={Confirm} />
-     <Route exact path="/End" component={End} />
+         <Route exact path="/" component={Home} />
+         <Route exact path="/Home" component={Home} />
+         <Route exact path="/Confirm" component={Confirm} />
+         <Route exact path="/End" component={End} />
         </Switch>
       </div>
     </ConnectedRouter>
