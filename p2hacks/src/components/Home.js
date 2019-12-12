c141211c3ab0f32e9beb67875b2265724e85c078
kiksun <kimera4173@gmail.com>
2019-12-12 10:17:25 +0900

    Confirm.jsとHome.jsへのページ遷移機能の実装

--- p2hacks/src/components/Home.js
+++ p2hacks/src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import { push } from 'connected-react-router'
+import { useDispatch } from 'react-redux';
 export default function Home() {
+    const dispatch = useDispatch();
+    const movetoQuestion = () => {
+        return( dispatch(push("/Question"))
+        )}
     return (
         <div>
             <p>
                 アンケートにご協力お願いします。
             </p>
-            <button >開始する</button>
+            <button onClick={movetoQuestion}>開始する</button>
         </div>
   );
 }
