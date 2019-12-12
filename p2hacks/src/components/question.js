ec48119d007e16ec5c79b5358af14e31516ca1d9
kiksun <kimera4173@gmail.com>
2019-12-12 15:19:58 +0900

    question.js内に条件文を追加して質問に10問回答したらConfirm.jsにページ遷移するようにした

--- p2hacks/src/components/question.js
+++ p2hacks/src/components/question.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import  addAnswer  from '../actions';
 import { connect } from 'react-redux';
 import Button from "@material-ui/core/Button";
-
 class Question extends Component {
 
   constructor(props) {
@@ -16,7 +15,7 @@ class Question extends Component {
   };
 
   render() {
-
+    const isQuestion = this.props.questionReduser.i;
     const question = [
       "Question1",
       "Question2",
@@ -31,14 +30,20 @@ class Question extends Component {
     ];
     return (
       <div>
-        <p>{question[this.props.questionReduser.i]}</p>
+        {(isQuestion<10) ? (
+           < >
+          <p>{question[this.props.questionReduser.i]}</p>
         <Button onClick={this.handleClick.bind(this, 5)}>そう</Button>
         <Button onClick={this.handleClick.bind(this, 4)}>そうかもしれない</Button>
         <Button onClick={this.handleClick.bind(this, 3)}>わからない</Button>
         <Button onClick={this.handleClick.bind(this, 2)}>そうではないかもしれない</Button>
         <Button onClick={this.handleClick.bind(this, 1)}>そうではない</Button>
-        {console.log(this.props.questionReduser.i)}
-        {console.log(this.props.answer)}
+        {/* {console.log(this.props.questionReduser.i)}
+            {console.log(this.props.answer)} */}
+            </>
+        ) : (
+            <Button onClick={ window.location.href = "/Confirm"}></Button>)
+        }
       </div>
     );
   }
