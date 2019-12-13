c9a50cf06a5bfe9b893b2c49d00bbce97a5a5c7f
kiksun <kimera4173@gmail.com>
2019-12-13 14:10:48 +0900

    fix modifed

--- p2hacks/src/store.js
+++ p2hacks/src/store.js
@@ -1,20 +1,21 @@
 import { createBrowserHistory } from 'history'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
-import rootReducer from './reducers'
+import createRootReducer from './reducers'
 
 export const history = createBrowserHistory()
 
 export default function configureStore(preloadedState) {
-    const  store  = createStore(
-        rootReducer(history), // root reducer with router state
-        preloadedState,
-        compose(
-            applyMiddleware(
-                routerMiddleware(history),
-            ),
-        ),
-    )
+  const store = createStore(
+    createRootReducer(history), // root reducer with router state
+    preloadedState,
+    compose(
+      applyMiddleware(
+        routerMiddleware(history), // for dispatching history actions
+        // ... other middlewares ...
+      ),
+    ),
+  )
 
-    return store
+  return store
 }
