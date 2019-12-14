be6cd8ae90fe73987ee900b4461e2e0a238e2ef8
kiksun <kimera4173@gmail.com>
2019-12-14 20:50:04 +0900

    cssファイル名の変更

--- p2hacks/src/store.js
+++ p2hacks/src/store.js
@@ -1,6 +1,14 @@
-import { createBrowserHistory } from 'history'
-import { applyMiddleware, compose, createStore } from 'redux'
-import { routerMiddleware } from 'connected-react-router'
+import {
+  createBrowserHistory
+} from 'history'
+import {
+  applyMiddleware,
+  compose,
+  createStore
+} from 'redux'
+import {
+  routerMiddleware
+} from 'connected-react-router'
 import createRootReducer from './reducers'
 import createSagaMiddleware from 'redux-saga';
 import rootSagas from "./sagas/index"
@@ -8,15 +16,15 @@ import rootSagas from "./sagas/index"
 export const history = createBrowserHistory()
 
 export default function configureStore(preloadedState) {
-  const sagaMiddleware=createSagaMiddleware()
+  const sagaMiddleware = createSagaMiddleware()
   const store = createStore(
     createRootReducer(history), // root reducer with router state
     preloadedState,
     compose(
       applyMiddleware(
-        routerMiddleware(history),
+        routerMiddleware(histgitory),
         sagaMiddleware
-         // for dispatching history actions
+        // for dispatching history actions
         // ... other middlewares ...
       ),
     ),
