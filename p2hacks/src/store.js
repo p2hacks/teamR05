20b2831f73c3238714818673c0d826eb7c7ac087
b1018043 <grey.fox.0417@gmail.com>
2019-12-14 18:11:54 +0900

    [chore]:aaa

--- p2hacks/src/store.js
+++ p2hacks/src/store.js
@@ -1,12 +1,11 @@
 import { createBrowserHistory } from 'history'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
-import createRootReducer from './reducers';
-import createSagaMiddleware from "redux-saga";
-import rootSaga from './sagas/index';
+import createRootReducer from './reducers'
+import createSagaMiddleware from 'redux-saga';
+import rootSagas from "./sagas/index"
 
 export const history = createBrowserHistory()
-
 
 export default function configureStore(preloadedState) {
   const sagaMiddleware = createSagaMiddleware();
@@ -15,11 +14,13 @@ export default function configureStore(preloadedState) {
     preloadedState,
     compose(
       applyMiddleware(
-        routerMiddleware(history), // for dispatching history actions
+        routerMiddleware(history),
         sagaMiddleware
+         // for dispatching history actions
+        // ... other middlewares ...
       ),
     ),
   )
-  sagaMiddleware.run(rootSaga);
+        sagaMiddleware.run(rootSagas)
   return store
 }
